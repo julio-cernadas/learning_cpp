7a6312ffad0fbc032edae190196bcd1bc547fc44
Cernadas <cernadas1021@gmail.com>
2019-01-25 17:32:48 -0500

    learning more

--- bjarne_book.cpp
+++ bjarne_book.cpp
@@ -40,7 +40,7 @@ int array_test()
     return 0;
 }
 
-int main() 
+void referencing_with_for_loop() 
 {
     int v[] = {0,1,2,3,4,5,6,7,8,9};
     for (int& x : v) {                  // Here we use a reference to x
@@ -50,5 +50,10 @@ int main()
     for (int x = 0; x != 10; ++x) {
         std::cout << v[x] << std::endl;
     }
+}
+
+int main() 
+{
+    
     return 0;
 }
