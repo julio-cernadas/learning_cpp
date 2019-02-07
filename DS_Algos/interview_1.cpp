caa9ed7b12794850ec901716bfbf2fb7a26a9307
Cernadas <cernadas1021@gmail.com>
2019-02-07 18:02:49 -0500

    moving onto DS and algos

--- DS_Algos/interview_1.cpp
+++ DS_Algos/interview_1.cpp
@@ -1,9 +1,50 @@
+#include<iostream>
+#include<sstream>
+#include<vector>
+#include<unordered_map>
 
-void stock_merchant() 
+void sock_merchant_map()                // Using unordered_map
 {
-    int n = 7;
-    int arr[] = {1,2,1,2,1,3,2}
+    int n, num;                         // number of sock in pile
+    std::cin >> n;                      // first line read
+    std::cin.ignore();
+    std::string input;
+    std::getline(std::cin, input);      // second line, sock colors
+    std::stringstream ss(input);
+    std::vector<int> socks;
+    while (ss >> num) {
+        socks.push_back(num);
+    }
+    std::unordered_map<int,int> counts;
+    for (const auto i : socks) {
+        counts[i]++;
+    }
+    int count = 0;
+    for (auto itr : counts) {
+        count += itr.second / 2;
+    }
+    std::cout << count;
+}
+
+void jumping_on_the_clouds()       
+{
+    int n,num;                          // number of clouds
+    std::cin >> n;                      // first line read
+    std::cin.ignore();
+    std::string input;
+    std::getline(std::cin, input);      // second line, sock colors
+    std::stringstream ss(input);
+    std::vector<int> clouds;
+    while (ss >> num) {
+        clouds.push_back(num);
+    }
+    for (int i = 0; i != clouds.size(); ++i) {
+        std::cout << i;
+    }
+}
 
 
-
+int main() 
+{
+    jumping_on_the_clouds();
 }
